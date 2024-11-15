7db75adba53d236d44fa98813c0b1e7aa2a650a8
Liang <2226975773@qq.com>
2024-11-15 13:23:58 +0800

    update GitHub pages

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "LiangWatcher-Nav",
   description: "A Nav Site.",
+  base: '/repo/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
